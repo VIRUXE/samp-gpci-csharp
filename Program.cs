f0e1fd718728b09da2efc6def99c7e710fd2681e
Flávio Pereira <1616657+VIRUXE@users.noreply.github.com>
2024-05-08 17:25:33 +0100

    the actual solution

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GPCI;
+
+internal class Program {
+    private static void Main(string[] args) {
+        string userHomePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        string gtaSanAndreasUserFiles = Path.Combine(userHomePath, "GTA San Andreas User Files");
+
+        var hash = GPCI.Calculate(gtaSanAndreasUserFiles[4..]);
+
+        Console.WriteLine($"GPCI Hash: {hash}");
+    }
+}
